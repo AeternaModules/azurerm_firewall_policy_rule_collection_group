410ce6254c26a8c1007be271663fc394b2cb46d0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "firewall_policy_rule_collection_groups" {
-  description = "All firewall_policy_rule_collection_group resources"
-  value       = azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups
-}
 output "firewall_policy_rule_collection_groups_application_rule_collection" {
-  description = "List of application_rule_collection values across all firewall_policy_rule_collection_groups"
-  value       = [for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : v.application_rule_collection]
+  description = "Map of application_rule_collection values across all firewall_policy_rule_collection_groups, keyed the same as var.firewall_policy_rule_collection_groups"
+  value       = { for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : k => v.application_rule_collection }
 }
 output "firewall_policy_rule_collection_groups_firewall_policy_id" {
-  description = "List of firewall_policy_id values across all firewall_policy_rule_collection_groups"
-  value       = [for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : v.firewall_policy_id]
+  description = "Map of firewall_policy_id values across all firewall_policy_rule_collection_groups, keyed the same as var.firewall_policy_rule_collection_groups"
+  value       = { for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : k => v.firewall_policy_id }
 }
 output "firewall_policy_rule_collection_groups_name" {
-  description = "List of name values across all firewall_policy_rule_collection_groups"
-  value       = [for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : v.name]
+  description = "Map of name values across all firewall_policy_rule_collection_groups, keyed the same as var.firewall_policy_rule_collection_groups"
+  value       = { for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : k => v.name }
 }
 output "firewall_policy_rule_collection_groups_nat_rule_collection" {
-  description = "List of nat_rule_collection values across all firewall_policy_rule_collection_groups"
-  value       = [for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : v.nat_rule_collection]
+  description = "Map of nat_rule_collection values across all firewall_policy_rule_collection_groups, keyed the same as var.firewall_policy_rule_collection_groups"
+  value       = { for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : k => v.nat_rule_collection }
 }
 output "firewall_policy_rule_collection_groups_network_rule_collection" {
-  description = "List of network_rule_collection values across all firewall_policy_rule_collection_groups"
-  value       = [for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : v.network_rule_collection]
+  description = "Map of network_rule_collection values across all firewall_policy_rule_collection_groups, keyed the same as var.firewall_policy_rule_collection_groups"
+  value       = { for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : k => v.network_rule_collection }
 }
 output "firewall_policy_rule_collection_groups_priority" {
-  description = "List of priority values across all firewall_policy_rule_collection_groups"
-  value       = [for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : v.priority]
+  description = "Map of priority values across all firewall_policy_rule_collection_groups, keyed the same as var.firewall_policy_rule_collection_groups"
+  value       = { for k, v in azurerm_firewall_policy_rule_collection_group.firewall_policy_rule_collection_groups : k => v.priority }
 }
 
